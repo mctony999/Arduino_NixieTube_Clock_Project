9ebfd9ececce6d674d52befb7dd6fe03ef705742
mctony999 <37092347+mctony999@users.noreply.github.com>
2019-07-15 22:24:25 +0800

    Improve the mapping Temperature number to 7-segment and show function
    Change the code in the "mapping Temperature number to 7-segment and show" part.
    Delete some unnecessary line and variable.

--- Clock_Prject/Clock_Prject.ino
+++ Clock_Prject/Clock_Prject.ino
@@ -11,11 +11,13 @@ DS3231 Clock;
 const long interval = 500;
 
 /**TM1637Display*/
-#define CLK 2
-#define DIO 3
+#define CLK A0
+#define DIO A1
+
 
 int colon_value = 1244; //For colon value 
 TM1637Display display(CLK, DIO);
+//TM1637Display display(A0,13);
 boolean colon = true;
 
 String tt = "";
@@ -24,7 +26,7 @@ String mm = "";
 String ss = "";
 
 /**Button*/
-const int buttonPin = 4; //the number of the pushbutton pin
+const int buttonPin = A2 ; //the number of the pushbutton pin
 boolean TempChangeShow = false;
 
 int buttonState; //previous stable state of the input pin
@@ -34,6 +36,11 @@ long debounceDelay = 50; //the debounce time; increase if the output flickers
 
 /*function*/
 boolean Button_Push_Check (int&, int&, int&, long&, long&, boolean&);
+
+/*Clock convert Array*/
+uint8_t D_Array[10] = {0b00111111,0b00000110,0b01011011,0b01001111,0b01100110,0b01101101,0b01111101,0b00000111,0b01111111,0b01101111};
+
+
 
 
 void setup() {
@@ -55,6 +62,9 @@ void setup() {
   }
 
   /**Button Setup*/
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+  
   pinMode(buttonPin,INPUT_PULLUP); //enable pullup resistor
   
   
@@ -63,32 +73,13 @@ void setup() {
 void loop() {
     
   // time update very second
-  unsigned long currentMillis = millis();
+  //unsigned long currentMillis = millis(); 
   boolean StoptheLoop = false;
-
   int reading = digitalRead(buttonPin);
   StoptheLoop = Button_Push_Check (reading, buttonState, lastButtonState,lastDebounceTime,debounceDelay, TempChangeShow);
   if (StoptheLoop == true){
     return;
   }
-  
-  /*old button function
-  if (reading != lastButtonState){ //button state changed
-    lastDebounceTime = millis(); //update last debounce time
-  }
-
-  
-  if ((millis() - lastDebounceTime) > debounceDelay){ //overtime
-    if (reading != buttonState) {//button state has changed
-    buttonState = reading; //update previous stable button state
-    if (buttonState == LOW){ // button presses
-      TempChangeShow =!TempChangeShow; //reverse the boolean
-    }
-    }
-  }
-  lastButtonState = reading; //update last button state
-  */
-
 
   //decide show Temperature or Time
   if (TempChangeShow == false) {
@@ -126,77 +117,8 @@ void loop() {
   }
   else {
     int TEMP = Clock.getTemperature(); //get temperature
-    Serial.print(TEMP);
-    Serial.print('\n');
-    
-    /**mapping Temperature number to 7-segment*/
-    uint8_t TEMP_1 = TEMP/10;
-    uint8_t TEMP_2 = TEMP%10;
-    //First number
-    if (TEMP_1==0){
-      TEMP_1 = 0b00111111;
-    }
-    else if (TEMP_1==1){
-      TEMP_1 = 0b00000110;
-    }
-    else if (TEMP_1==2){
-      TEMP_1 = 0b01011011;
-    }
-    else if (TEMP_1==3){
-      TEMP_1 = 0b01001111;
-    }
-    else if (TEMP_1==4){
-      TEMP_1 = 0b01100110;
-    }
-    else if (TEMP_1==5){
-      TEMP_1 = 0b01101101;
-    }
-    else if (TEMP_1==6){
-      TEMP_1 = 0b01111101;
-    }
-    else if (TEMP_1==7){
-      TEMP_1 = 0b00000111;
-    }
-    else if (TEMP_1==8){
-      TEMP_1 = 0b01111111;
-    }
-    else{
-     TEMP_1 = 0b01101111;
-      }
-    //second number
-    if (TEMP_2==0){
-      TEMP_2 = 0b00111111;
-    }
-    else if (TEMP_2==1){
-      TEMP_2 = 0b00000110;
-    }
-    else if (TEMP_2==2){
-      TEMP_2 = 0b01011011;
-    }
-    else if (TEMP_1==3){
-      TEMP_1 = 0b01001111;
-    }
-    else if (TEMP_2==4){
-      TEMP_2 = 0b01100110;
-    }
-    else if (TEMP_2==5){
-      TEMP_2 = 0b01101101;
-    }
-    else if (TEMP_2==6){
-      TEMP_2 = 0b01111101;
-    }
-    else if (TEMP_2==7){
-      TEMP_2 = 0b00000111;
-    }
-    else if (TEMP_2==8){
-      TEMP_2 = 0b01111111;
-    }
-    else{
-     TEMP_2 = 0b01101111;
-      }
-
-    /**Show Temperature*/
-    uint8_t data[] = {TEMP_1,TEMP_2,0b01100011,0b00111001};
+    /**mapping Temperature number to 7-segment and show*/
+    uint8_t data[] = {D_Array[TEMP/10],D_Array[TEMP%10],0b01100011,0b00111001};
     display.setSegments(data);
   }
 }
